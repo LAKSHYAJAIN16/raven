3374847d0013f65fee5a5acaeb052ad35e817e90
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2023-06-10 11:49:23 +0530

    'lol'

---
+++ ml/experiments/lda/lda.py
@@ -0,0 +1,131 @@
+import os
+import gensim
+import json
+import random
+import pandas as pd
+import numpy as np
+from gensim import corpora
+from nltk.corpus import stopwords
+
+# Vars
+DATASET = "Million News Headlines"
+stop_words = set(stopwords.words('english'))
+
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+
+def retrieve_all_posts_texts(n):
+    data = pd.read_csv(
+        r'D:\Projects\v3\raven\ml\experiments\topic-modelling\data\abcnews-date-text.csv')
+    docs = data["headline_text"][:n]
+    ret = []
+    for x in docs:
+        # tokenize text
+        ret.append(x)
+
+    return ret
+
+
+def create_fp(n, k):
+    return r"D:\Projects\v3\raven\ml\graphs\lda\lda-topics-" + str(n) + "-" + str(k) + r"-clusters.json"
+
+def create_model_fp(n, k):
+    os.mkdir(r"D:\Projects\v3\raven\ml\models\bad-lda-topics-" + str(n) + "-" + str(k) + r"-clusters")
+    return r"D:\Projects\v3\raven\ml\models\bad-lda-topics-" + str(n) + "-" + str(k) + r"-clusters\model"
+
+def main(n, k):
+    print("FUNCTION WAS CALLED WITH", n, "DOCS AND", k, "CLUSTERS")
+    documents = retrieve_all_posts_texts(n)
+
+    # Preprocess the documents by tokenizing and removing stopwords
+    stopwords = set('for a of the and to in'.split())
+    tokenized_docs = [[word for word in document.lower().split() if word not in stop_words]
+                      for document in documents]
+
+    # Create a dictionary from the preprocessed documents
+    dictionary = corpora.Dictionary(tokenized_docs)
+
+    # Convert tokenized documents into a document-term matrix
+    corpus = [dictionary.doc2bow(doc) for doc in tokenized_docs]
+
+    # Build the LDA model
+    num_topics = k
+    lda_model = gensim.models.LdaModel(
+        corpus=corpus, id2word=dictionary, num_topics=num_topics, iterations=200)
+
+    # Print the topics and their corresponding words
+    for idx, topic in lda_model.print_topics(num_topics=-1, num_words=10):
+        print(f'Topic #{idx} : {topic}')
+
+    # Get the topic distribution for a new document
+    new_doc = documents[random.randint(0, len(documents) - 1)]
+    new_doc_tokens = [word for word in new_doc.lower().split()
+                      if word not in stopwords]
+    new_doc_bow = dictionary.doc2bow(new_doc_tokens)
+    topic_distribution = lda_model.get_document_topics(new_doc_bow)
+
+    # Print the topic distribution
+    print("Topic Distribution for the new document:")
+    for topic in topic_distribution:
+        print(f"Topic #{topic[0]}: {topic[1]}")
+
+    topics = lda_model.show_topics(num_topics=-1)
+    perplexity = lda_model.log_perplexity(corpus)
+    lda_model.save(create_model_fp(len(documents), num_topics))
+
+    # now, find the number of empty topics (i.e all probs are zero)
+    empty_topics = []
+    for f in range(len(topics)):
+        targ = topics[f][1]
+        offsets = targ.split(' + ')
+        n = 0
+        t = 0
+        for g in offsets:
+            fin = float(g.split("*")[0])
+            if fin <= 0.000000000000000000000001:
+                n += 1
+            t += 1
+
+        if n/t >= 0.6:
+            empty_topics.append(topics[f])
+
+    return_obj = {
+        "meta": {
+            "nDocuments": len(documents),
+            "dataset": DATASET,
+            "k": num_topics,
+            "perplexity": perplexity,
+            "empty_amount": len(empty_topics),
+        },
+        "topics": topics,
+        "empty_topics": empty_topics,
+    }
+    file = open(create_fp(len(documents), k), "w")
+    file.write(json.dumps(return_obj, cls=NpEncoder))
+    file.close()
+    print("PERPLEXITY", perplexity)
+
+
+MAX_N = 100000
+MAX_K = 100
+MIN_N = 5000
+MIN_K = 10
+K_STEP = 10
+N_STEP = 10000
+nn = MIN_N
+
+while nn < MAX_N:
+    nk = MIN_K
+    while nk < MAX_K:
+        main(nn, nk)
+        nk += K_STEP
+    nn += N_STEP
--- ml/experiments/topic-modelling/subclasses/modelling-million-news.py
+++ ml/experiments/topic-modelling/subclasses/modelling-million-news.py
@@ -3,53 +3,59 @@ from bertopic import BERTopic
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-INSTANCE="20k"
 stop_words = set(stopwords.words('english'))
 
-def create_fp(index):
-    return r"D:\Projects\v3\raven\ml\graphs\tm-n-" + str(INSTANCE) + "-" + str(index) + r".html"
+def main(instance, n):
+    def create_fp(index):
+        return r"D:\Projects\v3\raven\ml\graphs\modelling\tm-n-" + str(instance) + "k-" + str(index) + r".html"
+    
+    def retrieve_all_posts_texts():
+        data = pd.read_csv(r'D:\Projects\v3\raven\ml\experiments\topic-modelling\data\abcnews-date-text.csv')
+        docs = data["headline_text"][:n]
+        ret = []
+        i = 0
+        for x in docs:
+            # tokenize text
+            tokens = word_tokenize(x)
+            filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
+            final = " ".join(filtered_sentence)
+            ret.append(final)
+            i += 1
+            if i % 1000 == 0:
+                print("Epoch",i,"successful")
+    
+        return ret
+    
+    # retrieve all posts
+    print("STARTING",instance,"NOW")
+    docs = retrieve_all_posts_texts()
+    print("len", len(docs))
+    
+    # create model
+    topic_model = BERTopic(verbose=True, calculate_probabilities=True)
+    topics, probs = topic_model.fit_transform(docs)
+    print(topic_model.get_topic_info())
+    
+    # a bunch of visualizations for debugging
+    foc = topic_model.visualize_topics()  
+    foc2 = topic_model.visualize_heatmap()
+    foc3 = topic_model.visualize_barchart()
+    foc5 = topic_model.visualize_documents(docs)
+    foc6 = topic_model.visualize_hierarchy()
+    foc.write_html(create_fp("1"))
+    foc2.write_html(create_fp("2"))
+    foc3.write_html(create_fp("3"))
+    foc5.write_html(create_fp("4"))
+    foc6.write_html(create_fp("5"))
+    
+    # while True:
+    #     x = input("Enter search string")
+    #     topics, probs= topic_model.transform([x])
+    #     print(topics)
+    #     print(probs)
 
-def retrieve_all_posts_texts():
-    data = pd.read_csv(r'D:\Projects\v3\raven\ml\experiments\topic-modelling\data\abcnews-date-text.csv')
-    docs = data["headline_text"][:20000]
-    ret = []
-    i = 0
-    for x in docs:
-        # tokenize text
-        tokens = word_tokenize(x)
-        filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
-        final = " ".join(filtered_sentence)
-        ret.append(final)
-        i += 1
-        if i % 1000 == 0:
-            print("Epoch",i,"successful")
+    # reccursion
+    print("INSTANCE",instance,"DONE")
+    main(instance + 10, n + 10000)
 
-    return ret
-
-# retrieve all posts
-docs = retrieve_all_posts_texts()
-print("len", len(docs))
-
-# create model
-topic_model = BERTopic(verbose=True, calculate_probabilities=True)
-topics, probs = topic_model.fit_transform(docs)
-print(topic_model.get_topic_info())
-
-# a bunch of visualizations for debugging
-foc = topic_model.visualize_topics()  
-foc2 = topic_model.visualize_heatmap()
-foc3 = topic_model.visualize_barchart()
-foc5 = topic_model.visualize_documents(docs)
-foc6 = topic_model.visualize_hierarchy()
-foc.write_html(create_fp("1"))
-foc.write_
-foc2.write_html(create_fp("2"))
-foc3.write_html(create_fp("3"))
-foc5.write_html(create_fp("4"))
-foc6.write_html(create_fp("5"))
-
-while True:
-    x = input("Enter search string")
-    topics, probs= topic_model.transform([x])
-    print(topics)
-    print(probs)
+main(40,40000)
